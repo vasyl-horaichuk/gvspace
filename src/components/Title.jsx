6b09048712c5b077c2f7ba3c67189653273016cd
Vasyl Horaichuk <vg.createam@gmail.com>
2023-09-24 23:02:22 +0300

    update

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,10 +2,10 @@ import { TitleName, TextName, TintName } from './TitleName.styled';
 
 export const Title = ({ title, text, caption }) => {
   return (
-    <div>
+    <>
       <TitleName>{title}</TitleName>
       <TextName>{text}</TextName>
       <TintName>{caption}</TintName>
-    </div>
+    </>
   );
 };
